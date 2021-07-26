beb85878b2d8a49a82f90ff31f4f5697cdf424e2
segara2410 <segara2410@gmail.com>
2021-07-26 12:17:16 +0000

    Deploying to gh-pages from @ ichiro-its/keisan@9bd7f12335bc51ae69b0c88abecf9525b34b1fbe 🚀

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['clamp_142',['clamp',['../namespacekeisan.html#a22bbadee975f63efe26f4f0398f923a8',1,'keisan']]],
+  ['clamp_142',['clamp',['../namespacekeisan.html#a5fb8c7b22016276ba9e2f658876ba908',1,'keisan']]],
   ['clamp_5fnumber_143',['clamp_number',['../namespacekeisan.html#a844aa2b8c0c9e01ab8ee707e09e896fa',1,'keisan']]],
   ['cos_144',['cos',['../namespacekeisan.html#ac6a97fa573764b02a8f859135e7bf69a',1,'keisan']]],
   ['cross_5fproduct_145',['cross_product',['../namespacekeisan.html#a6f5caed66f17af5af83304d908c9362d',1,'keisan::cross_product(const Point2 &amp;a, const Point2 &amp;b)'],['../namespacekeisan.html#a53f0ed359db4bb8c9441d383f260e9e8',1,'keisan::cross_product(const Point3 &amp;a, const Point3 &amp;b)']]]
--- search/functions_c.js
+++ search/functions_c.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['scale_201',['scale',['../structkeisan_1_1Point2.html#aae157b3d50fae6ab3576a295af964ab4',1,'keisan::Point2::scale(const double &amp;scaling) const'],['../structkeisan_1_1Point2.html#ae6be30448c82a7c8d79b2a49cc961338',1,'keisan::Point2::scale(const Point2 &amp;scaling) const'],['../namespacekeisan.html#a87c45429a3475d8765b67e008be8b15e',1,'keisan::scale()']]],
+  ['scale_201',['scale',['../structkeisan_1_1Point2.html#aae157b3d50fae6ab3576a295af964ab4',1,'keisan::Point2::scale(const double &amp;scaling) const'],['../structkeisan_1_1Point2.html#ae6be30448c82a7c8d79b2a49cc961338',1,'keisan::Point2::scale(const Point2 &amp;scaling) const'],['../namespacekeisan.html#ad4ff75f05af2ee11edbb436ff8b06171',1,'keisan::scale()']]],
   ['scale_5ffrom_202',['scale_from',['../structkeisan_1_1Point2.html#ad33198a2b12d5affce3b6558925bfe62',1,'keisan::Point2::scale_from(const Point2 &amp;scaling, const Point2 &amp;anchor)'],['../structkeisan_1_1Point2.html#aa33dd600cdcfebfd04562bcd50fc6dbc',1,'keisan::Point2::scale_from(const double &amp;scaling, const Point2 &amp;anchor)']]],
   ['scale_5fnumber_203',['scale_number',['../namespacekeisan.html#a0ea5bf910fa081e4a02fec51b5d8c1a8',1,'keisan']]],
   ['set_5frotation_204',['set_rotation',['../classkeisan_1_1Transform2.html#a69c55746bf164097b1bb02cf1625bcca',1,'keisan::Transform2']]],
   ['set_5fscale_205',['set_scale',['../classkeisan_1_1Transform2.html#afb38fb282472687c5b4631d7ec186274',1,'keisan::Transform2::set_scale(const Point2 &amp;scale)'],['../classkeisan_1_1Transform2.html#a0554e310f74fd2c4879309965a442143',1,'keisan::Transform2::set_scale(const double &amp;scale)']]],
   ['set_5ftranslation_206',['set_translation',['../classkeisan_1_1Transform2.html#aa2ffbb8c6178f50ec2b08a4f7304cb00',1,'keisan::Transform2']]],
-  ['sign_207',['sign',['../namespacekeisan.html#a5cf1b3573511a36cb69678b056bfca3f',1,'keisan']]],
+  ['sign_207',['sign',['../namespacekeisan.html#ab0e763b09c2e4b663b87b401e51165fc',1,'keisan']]],
   ['sign_5fnumber_208',['sign_number',['../namespacekeisan.html#acca34695effd6e2ebe4408fb1c0f3da2',1,'keisan']]],
   ['signed_5farctan_209',['signed_arctan',['../namespacekeisan.html#a646d84ae73153a968a750e041d22c6e5',1,'keisan']]],
   ['sin_210',['sin',['../namespacekeisan.html#a110e770ce7ad62165b9560fe847b4a39',1,'keisan']]],

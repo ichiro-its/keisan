808b738e8941165adc7d99ce3127c4a3b3fc982d
FaaizHaikal <115574690+FaaizHaikal@users.noreply.github.com>
2025-03-21 17:08:48 +0000

    Deploying to gh-pages from @ ichiro-its/keisan@ea685d459e61931b33576b6d40439daa2f1030bc 🚀

--- search/all_d.js
+++ search/all_d.js
@@ -5,6 +5,6 @@ var searchData=
   ['roll_99',['roll',['../structkeisan_1_1Euler.html#a9fbe35e998d0931bff9994bd4d4053b0',1,'keisan::Euler']]],
   ['rotate_100',['rotate',['../structkeisan_1_1Point2.html#a1dfc8da21b58a29f1e9b6349a7f299e6',1,'keisan::Point2']]],
   ['rotate_5ffrom_101',['rotate_from',['../structkeisan_1_1Point2.html#ac46ae42f438920495310935654ba0a52',1,'keisan::Point2']]],
-  ['rotation_5fmatrix_102',['rotation_matrix',['../namespacekeisan.html#a332cc3fab9f47b1b292bec82c5775bd2',1,'keisan::rotation_matrix(const Angle&lt; double &gt; &amp;angle)'],['../namespacekeisan.html#ae9858b1f3b80093ad91bf78b3b6edd90',1,'keisan::rotation_matrix(const Euler&lt; double &gt; &amp;angle)']]],
+  ['rotation_5fmatrix_102',['rotation_matrix',['../namespacekeisan.html#a332cc3fab9f47b1b292bec82c5775bd2',1,'keisan::rotation_matrix(const Angle&lt; double &gt; &amp;angle)'],['../namespacekeisan.html#ae9858b1f3b80093ad91bf78b3b6edd90',1,'keisan::rotation_matrix(const Euler&lt; double &gt; &amp;angle)'],['../namespacekeisan.html#aa0d55f01093ebdb207f80820586d77a7',1,'keisan::rotation_matrix(const Quaternion&lt; double &gt; &amp;quaternion)']]],
   ['round_103',['round',['../classkeisan_1_1Matrix.html#a15fa8cea5f23982e5068c0ec7710e8f1',1,'keisan::Matrix']]]
 ];
--- search/functions_d.js
+++ search/functions_d.js
@@ -3,6 +3,6 @@ var searchData=
   ['radian_235',['radian',['../classkeisan_1_1Angle.html#a3dcda6435a699be62b639d4eb56dce9a',1,'keisan::Angle']]],
   ['rotate_236',['rotate',['../structkeisan_1_1Point2.html#a1dfc8da21b58a29f1e9b6349a7f299e6',1,'keisan::Point2']]],
   ['rotate_5ffrom_237',['rotate_from',['../structkeisan_1_1Point2.html#ac46ae42f438920495310935654ba0a52',1,'keisan::Point2']]],
-  ['rotation_5fmatrix_238',['rotation_matrix',['../namespacekeisan.html#a332cc3fab9f47b1b292bec82c5775bd2',1,'keisan::rotation_matrix(const Angle&lt; double &gt; &amp;angle)'],['../namespacekeisan.html#ae9858b1f3b80093ad91bf78b3b6edd90',1,'keisan::rotation_matrix(const Euler&lt; double &gt; &amp;angle)']]],
+  ['rotation_5fmatrix_238',['rotation_matrix',['../namespacekeisan.html#a332cc3fab9f47b1b292bec82c5775bd2',1,'keisan::rotation_matrix(const Angle&lt; double &gt; &amp;angle)'],['../namespacekeisan.html#ae9858b1f3b80093ad91bf78b3b6edd90',1,'keisan::rotation_matrix(const Euler&lt; double &gt; &amp;angle)'],['../namespacekeisan.html#aa0d55f01093ebdb207f80820586d77a7',1,'keisan::rotation_matrix(const Quaternion&lt; double &gt; &amp;quaternion)']]],
   ['round_239',['round',['../classkeisan_1_1Matrix.html#a15fa8cea5f23982e5068c0ec7710e8f1',1,'keisan::Matrix']]]
 ];
